07000463e6f9e71dbf99c588854ca383e5b601ad
nafey7 <nafeymoiz07@gmail.com>
2022-10-30 10:28:16 +0500

    allowing patch and delete

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
     if (allowedOrigins.includes(origin)) {
          res.setHeader('Access-Control-Allow-Origin', origin);
     }
-    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
     return next();
